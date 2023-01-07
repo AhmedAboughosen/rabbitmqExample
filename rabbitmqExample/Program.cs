173e9e8b65e7d426572b0a0890ee609f31772d84
Ahmed Aboughosen <55014628+AhmedAboughosen@users.noreply.github.com>
2023-01-07 20:51:58 +0200

    Initial commit

---
+++ rabbitmqExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rabbitmqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
